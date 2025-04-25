9c6c04e62c803f097569dbc185a906dd9a2fa357
Mouhamedmane123456789 <mouhamedmane644@gmail.com>
2025-04-25 22:51:50 +0000

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,17 @@
+// Animation au scroll (fade-in)
+const sections = document.querySelectorAll('section');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show-section');
+    }
+  });
+}, {
+  threshold: 0.1
+});
+
+sections.forEach(section => {
+  section.classList.add('hidden-section');
+  observer.observe(section);
+});
